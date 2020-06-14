41ce69d1ac2ae2c247b89477c571fd45b0ab19cd
DragonFenixOwi <mecaowi@gmail.com>
2020-06-14 15:25:55 +0200

    Orden de Estructura y comentarios

--- src/Promesas/Promesa_API.js
+++ src/Promesas/Promesa_API.js
@@ -46,19 +46,16 @@ function traerDatos(url_api)
                     {  
                         
                         if(xhttp.readyState ===4) // 4to Estado. Indica si se ha completado el "request". Si es completado entra al if
-                            {
-                                                                
-                                    if(xhttp.status===200) // Estado 200. Validamos si es correcta la petición del servidor
-                                            {
-                                                    resolve(JSON.parse(xhttp.responseText));
-                                            }
-                                        else  
-                                            {
-                                                let  error = new Error("Error" + url_api); 
-                                                return reject(error); 
-                                            };
-                                
-                                    
+                            {                                                           
+                                if(xhttp.status===200) // Estado 200. Validamos si es correcta la petición del servidor
+                                    {
+                                        resolve(JSON.parse(xhttp.responseText)); // Pareamos  
+                                    }
+                                else  
+                                    {
+                                        let  error = new Error("Error" + url_api); 
+                                        return reject(error); 
+                                    };                                                                   
                             };
                         };xhttp.send(); //Envio de petición  
                 }); //Nota: No enviaba datos porque el metodo ".send" estaba afuera de la promesa
@@ -78,30 +75,39 @@ function traerDatos(url_api)
 traerDatos(API)
     .then ((dato_1) =>
         {
+            //Se Imprime en consola 
             console.log(`\t Primeros Datos ".then" `);
             console.log(`\t - ${dato_1.info.count}`);
             console.log(`\t - ${dato_1.info.pages}`);
+            // Se envia la siguiente petición
             return traerDatos (API + dato_1.results[0].id);
         })
+
     .then ((dato_2) =>
         {
+            //Se Imprime en consola 
             console.log(`\n `);
             console.log(`\t Segundos Datos ".then" :\n `);
             console.log(`\t - ${dato_2.name}`);
             console.log(`\t - ${dato_2.status}`);
             console.log(`\t - ${dato_2.species}`);
             console.log(`\t - ${dato_2.gender}`);
+            // Se envia la siguiente petición
             return traerDatos (dato_2.origin.url);
-        })    
+        })
+
     .then ((dato_3) =>
         {
+            //Se Imprime en consola 
             console.log(`\n `);
             console.log(`\t Tercer Dato ".then" :\n `);
             console.log(`\t - ${dato_3.name}`);
             console.log(`\t - ${dato_3.type}`);
             console.log(`\t - ${dato_3.dimension}`);
+        })
 
-        })    
-    .catch ((error) => {
-        console.error(error);
-    }); 
+    .catch ((error) => 
+        {
+            //Se Imprime en consola
+            console.error(error);
+        }); 
